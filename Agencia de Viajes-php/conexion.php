0881cbf317029bb778f335085be109f888c71913
Darío Barboso <61385165+dariovtx@users.noreply.github.com>
2021-08-25 12:34:04 -0300

    Delete Agencia de Viajes-php directory

--- Agencia de Viajes-php/conexion.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conexion=mysqli_connect("localhost","root","","bdd_agencia_viajes")or die ("Problemas de conexión");
-?>
--- Agencia de Viajes-php/salir.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-session_start();
-header("cacge-control: private");
-session_unset();
-session_destroy();
-header("location: login.php");
-echo "<html></html>";
-exit;
-?>
